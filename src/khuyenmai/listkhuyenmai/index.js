9ec6a4e8b09e53dc5a6ec265e1b21ba5afeb54e6
vantich <49013451+vantich@users.noreply.github.com>
2020-06-28 14:54:41 +0700

    lô

--- src/khuyenmai/listkhuyenmai/index.js
+++ src/khuyenmai/listkhuyenmai/index.js
@@ -5,7 +5,8 @@ import {
   Text,
   View,
   Dimensions,
-  ScrollView
+  ScrollView,
+  TouchableOpacity
 } from 'react-native'; 
 const Listkhuyenmai = () => 
 {
@@ -14,51 +15,61 @@ const Listkhuyenmai = () =>
   return(
     <ScrollView>
     <View>
-      {/* <View style={{width:'100%',height:screenHeight/3}} >
-          <Image style={{width:'100%',height:'100%',position:'relative' } }source={require('../../assets/images/tai-nghe-jbl-t500btblk.jpg')}>
-          </Image>
-          <Text style={{position:'absolute',color:'red',fontSize:30,right:10,top:100,fontFamily:'Open Sans'}}>SALE!</Text>
-          <View style={{width:'100%',height:screenHeight/20}}>
-            <Text style={styles.title}>
-              Khuyến mãi hơn cả khuyến mãi 
-            </Text>
+      <View style={{flex:1, flexDirection: 'row',marginTop:20}}>
+        <View style={{width:'45%',height:screenHeight/3,left:10,borderRadius: 10,
+    shadowColor:'black',shadowOpacity:10,elevation: 3}}>
+          <View style={{width:'97%',height:'70%',left:2}} >
+            <Image style={{width:'100%',height:'100%',borderRadius: 10,position:'relative' } }source={require('../../assets/images/tai-nghe-jbl-t500btblk.jpg')}></Image>
+            <Text style={{position:'absolute',color:'red',fontSize:30,right:10,top:100,fontFamily:'Open Sans'}}>SALE!</Text>    
           </View>
-      </View> */}
-      <View style={{width:'100%',height:screenHeight/3,paddingTop:40}}>
-        <Image style={{width:'100%',height:'100%',position:'relative' } }source={require('../../assets/images/Loa-bose-Soundlink-mini.jpg')}>
-            </Image>
-            <Text style={{position:'absolute',color:'white',transform:[{rotate:"-20deg"}],fontSize:22,left:120,bottom:90,fontFamily:'Open Sans'}}>SALE UP TO 50%</Text>
+          <View style={{width:'95%',left:20}}>
+            <TouchableOpacity style={{marginTop:10,height:45}}>
+              <Text style={{color: "black",fontSize:20,}}>Tai nghe jbl </Text>  
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{width:'45%',height:screenHeight/3,left:20,borderRadius: 10,
+    shadowColor:'black',shadowOpacity:10,elevation: 3}}>
+          <View style={{width:'97%',height:'70%',left:2}}>
+            <Image style={{width:'100%',height:'100%',borderRadius: 10,position:'relative' } }source={require('../../assets/images/Loa-bose-Soundlink-mini.jpg')}></Image>
+            <Text style={{position:'absolute',color:'white',transform:[{rotate:"-20deg"}],fontSize:22,bottom:90,fontFamily:'Open Sans'}}>SALE UP TO 50%</Text>
+          </View>
+          <View style={{width:'95%',left:20}}>
+            <TouchableOpacity style={{marginTop:10,height:45}}>
+              <Text style={{color: "black",fontSize:20,}}>Loa bose Soundlink mini </Text>  
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-      {/* <View style={{width:'100%',height:screenHeight/2,paddingTop:40}}>
-        <Image style={{width:'100%',height:'100%',position:'relative' } }source={require('../../assets/images/airpods.jpg')}>
-              </Image>
-              <Text style={{position:'absolute',color:'red',top:50,fontSize:34}}>Big sale</Text>
-      </View> */}
+      <View style={{flex:1,flexDirection: 'row',marginTop:20}}>
+        <View style={{width:'45%',height:screenHeight/3,left:10,borderRadius: 10,
+    shadowColor:'black',shadowOpacity:10,elevation: 3}}>
+          <View style={{width:'97%',height:'70%',left:2}}>
+            <Image style={{width:'100%',height:'100%',borderRadius: 10,position:'relative' } }source={require('../../assets/images/airpods.jpg')}></Image>
+            <Text style={{position:'absolute',color:'red',top:50,fontSize:34}}>Big sale</Text>
+          </View>
+          <View style={{width:'95%',left:20}}>
+            <TouchableOpacity style={{marginTop:10,height:45}}>
+              <Text style={{color: "black",fontSize:20,}}>Tai nghe airpods </Text>  
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{width:'45%',height:screenHeight/3,left:20,borderRadius: 10,
+    shadowColor:'black',shadowOpacity:10,elevation: 3}}>
+          <View style={{width:'97%',height:'70%',left:2}}>
+            <Image style={{width:'100%',height:'100%',borderRadius: 10,position:'relative' } }source={require('../../assets/images/tai-nghe-iphone-5.jpg')}></Image>
+            <Text style={{position:'absolute',color:'red',top:50,fontSize:24}}>Sale 20%</Text>
+          </View>
+          <View style={{width:'95%',left:20}}>
+            <TouchableOpacity style={{marginTop:10,height:45}}>
+              <Text style={{color: "black",fontSize:20,}}>Tai nghe jbl </Text>  
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-    {/* <View style={{width:'100%',height:screenHeight/2,paddingTop:40}}>
-      <View style={{width:'100%', height:'20%'}}>
-        <Text> Loa bluetooth</Text>
-      </View>
-      <View style={{width:'100%', height:'80%'}}>
-        <View style={{width:'100%', height:'50%'}}>
-            <View style={{width:'50%', height:'100%',alignItems:'center'}} >
-              <View style={{width:'90%', height:'90%'}} >
-                <Image style={{width:'100%',height:'100%' } }source={require('../../assets/images/Loa-bose-Soundlink-mini.jpg')}> </Image>
-              </View>
-            </View>
-            <View  style={{width:'50%', height:'100%'}}>
-            <View style={{width:'90%', height:'90%'}} >
-                <Image style={{width:'100%',height:'100%' } }source={require('../../assets/images/Loa-bose-Soundlink-mini.jpg')}> </Image>
-              </View>
-            </View>
-        </View>
-        <View style={{width:'100%', height:'50%'}}>
-          <View style={{width:'50%', height:'100%'}} ></View>
-          <View  style={{width:'50%', height:'100%'}}></View>
-        </View>
-      </View>
-    </View> */}
     </ScrollView>
   );
 };
+
 export default Listkhuyenmai;
